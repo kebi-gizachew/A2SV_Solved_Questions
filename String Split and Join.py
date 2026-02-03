52c01a146a4bd00e6c806f0a9b089754f2c20ade
Kebron <kebisel21@gmail.com>
2026-02-03 14:49:38 +0300

    Implement split_and_join function to replace spaces

---
+++ String Split and Join.py
@@ -0,0 +1,12 @@
+
+
+def split_and_join(line):
+    # write your code here
+    l=line.split(" ")
+    t="-".join(l)
+    return t
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
