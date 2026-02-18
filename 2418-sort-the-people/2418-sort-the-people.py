b06c155f448b6067780b7283100466c793f71ab6
Kebron <kebisel21@gmail.com>
2026-02-18 18:37:23 +0300

    Time: 4 ms (64.83%), Space: 19.7 MB (42.31%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -5,14 +5,19 @@ class Solution:
         for i in heights:
             d[i] = names[l]
             l+=1
-        for i in range(len(heights)):
-            for j in range(len(heights)-i - 1):
-                if heights[j] < heights[j + 1]:
-                    heights[j + 1] , heights[j]=heights[j], heights[j+1]
-        arr=[]
+        arr =[]
+        heights.sort(reverse=True)
         for k in heights:
             arr.append(d[k])
         return arr
+        # for i in range(len(heights)):
+        #     for j in range(len(heights)-i - 1):
+        #         if heights[j] < heights[j + 1]:
+        #             heights[j + 1] , heights[j]=heights[j], heights[j+1]
+        # arr=[]
+        # for k in heights:
+        #     arr.append(d[k])
+        # return arr
         
                 
         
