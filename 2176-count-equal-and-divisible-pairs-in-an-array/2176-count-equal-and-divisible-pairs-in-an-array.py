8f166d9d10cbc2763d04fc426838a0f2543f666a
Kebron <kebisel21@gmail.com>
2026-02-19 11:02:57 +0300

    Time: 6 ms (96.5%), Space: 19.6 MB (15.73%) - LeetHub

--- 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.py
+++ 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.py
@@ -1,18 +1,35 @@
 class Solution:
     def countPairs(self, nums: List[int], k: int) -> int:
-        d = defaultdict(list)
-        op =0
-        if not nums:
-            return 0
-        for i , j in enumerate(nums):
-            d[j].append(i)
-        for n , m in d.items():
-            if len(m) > 1:
-                for j in range(len(m)):
-                    for y in range(j+1,len(m)):
-                        if m[j] * m[y]% k ==0:
-                            op += 1
+        d = defaultdict(lambda: defaultdict(int))
+        op = 0
+        for i in range(len(nums)):
+            g = gcd(k , i)
+            if nums[i] in d:
+                g = gcd(k, i)
+                for t in d[nums[i]]:
+                    if (g * t) % k == 0:
+                        op+=d[nums[i]][t]
+            d[nums[i]][g] += 1
         return op
+
+                
+
+
+
+
+        # d = defaultdict(list)
+        # op =0
+        # if not nums:
+        #     return 0
+        # for i , j in enumerate(nums):
+        #     d[j].append(i)
+        # for n , m in d.items():
+        #     if len(m) > 1:
+        #         for j in range(len(m)):
+        #             for y in range(j+1,len(m)):
+        #                 if m[j] * m[y]% k ==0:
+        #                     op += 1
+        # return op
 
 
 
