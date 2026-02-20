a7b6512f33d61d0ae8d55ee1eb471e4b702c3c83
Kebron <kebisel21@gmail.com>
2026-02-20 23:53:10 +0300

    Time: 0 ms (100%), Space: 19.3 MB (52.39%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -1,22 +1,14 @@
-class Solution(object):
-    def largestNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: str
-        """
-        n=list(map(str,nums))
-        def order(x,y):
-            if x+y<y+x:
-                return 1
-            if x+y>y+x:
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        s = list(map(str, nums))
+        def order(x , y):
+            if x + y > y + x:
                 return -1
+            elif x+ y<y+x:
+                return 1
             else:
                 return 0
-        n.sort(key=cmp_to_key(order))
-        k= ''.join(n)
-        return "0" if k[0]=="0" else k
-
-
-      
+        s.sort(key = cmp_to_key(order))
+        return "".join(s) if s[0] != "0" else "0"
         
-       
+        
