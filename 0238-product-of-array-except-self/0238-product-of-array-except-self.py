79163bbe4f744d59a66db109b59e840f65c171f0
Kebron <kebisel21@gmail.com>
2026-03-04 19:29:53 +0300

    Time: 43 ms (11.78%), Space: 26.8 MB (31.37%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,19 +1,17 @@
-class Solution(object):
-    def productExceptSelf(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        prod, zero_cnt = 1, 0
-        for num in nums:
-            if num:
-                prod *= num
-            else:
-                zero_cnt +=  1
-        if zero_cnt > 1: return [0] * len(nums)
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        pre = [1] *len(nums)
+        suf = [1]*len(nums)
+        finale= []
+        for i in range(1 , len(nums)):
+            pre[i] = pre[i - 1] * nums[i - 1]
+            suf[len(nums)-i - 1] = suf[len(nums) -i] * nums[len(nums) -i]       
+        for t in range(len(nums)):
+            finale.append(pre[t] * suf[t])
+        return finale
+        
 
-        res = [0] * len(nums)
-        for i, c in enumerate(nums):
-            if zero_cnt: res[i] = 0 if c else prod
-            else: res[i] = prod // c
-        return res
+
+
+
+        
