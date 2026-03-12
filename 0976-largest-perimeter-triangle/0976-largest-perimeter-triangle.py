f8a2ad262b62ef51891c82cd49fecde3bf947e0b
Kebron <kebisel21@gmail.com>
2026-03-12 19:43:14 +0300

    Time: 18 ms (33.52%), Space: 20.4 MB (92.88%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.py
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.py
@@ -3,10 +3,14 @@ class Solution:
         nums.sort()
         if len(nums) < 3:
             return 0
-        l , r = len(nums)-3, len(nums) - 1
-        while l>=0:
-            if nums[l] + nums[l+1] > nums[r]:
-                return nums[l] + nums[l+1] + nums[r]
-            l -= 1
-            r -= 1
+        for r in range(len(nums) - 3 , -1, -1):
+            if nums[r] + nums[r + 1] > nums[r + 2]:
+                return nums[r] + nums[r + 1] + nums[r + 2]
         return 0
+        # l , r = len(nums)-3, len(nums) - 1
+        # while l>=0:
+        #     if nums[l] + nums[l+1] > nums[r]:
+        #         return nums[l] + nums[l+1] + nums[r]
+        #     l -= 1
+        #     r -= 1
+        # return 0
