7ad54a5e819d838707de88d0e0833f96bb238b61
Kebron <kebisel21@gmail.com>
2026-03-21 17:27:09 +0300

    Time: 0 ms (100%), Space: 19.4 MB (61.54%) - LeetHub

--- 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.py
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.py
@@ -1,19 +1,48 @@
 class Solution:
     def minMoves(self, target: int, maxDoubles: int) -> int:
         count = 0
-        def trial(res):
-            nonlocal count , maxDoubles
-            if res == 1:
-                return
-            if res % 2== 0 and maxDoubles != 0:
-                maxDoubles -= 1
-                count += 1
-                trial(res // 2)
+        stack = [target]
+        while target > 1:
+            if maxDoubles == 0:
+                return count + target - 1
+            if target % 2 == 0:
+                target = target // 2
+                maxDoubles -=1 
             else:
-                count += 1
-                trial(res - 1)
-        trial(target)
+                target -= 1
+            count += 1
         return count
+
+
+
+
+
+        # while stack:
+        #     temp = stack.pop()
+        #     if maxDoubles == 0:
+        #         count += (temp - 1)
+        #         break
+        #     elif temp % 2 == 0:
+        #         stack.append(temp// 2) if not temp == 1 else pass
+        #         maxDoubles -=1 
+        #     else:
+        #         stack.append(temp - 1)
+        #     count += 1
+        # return count
+
+        # # def trial(res):
+        # #     nonlocal count , maxDoubles
+        # #     if res == 1:
+        # #         return
+        # #     if res % 2== 0 and maxDoubles != 0:
+        # #         maxDoubles -= 1
+        # #         count += 1
+        # #         trial(res // 2)
+        # #     else:
+        # #         count += 1
+        # #         trial(res - 1)
+        # # trial(target)
+        # # return count
 
 
 
