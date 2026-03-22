e1d6cc0edc36ad210d3b4d7314e38b30d4ac9cb0
Kebron <kebisel21@gmail.com>
2026-03-23 00:54:59 +0300

    Time: 0 ms (100%), Space: 19.4 MB (93.86%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,6 +1,17 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return pow(x , n)
+        def power(num ,times):
+            res = 1
+            while times > 0:
+                if times % 2 == 1:
+                    res *=num
+                num *= num
+                times //= 2
+            return res
+        if n < 0:
+            x = 1/x
+            n = -n
+        return power(x , n)
         
 
 
