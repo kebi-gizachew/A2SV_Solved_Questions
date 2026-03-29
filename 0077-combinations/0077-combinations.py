9a99d64dfad867ec8b1ea3d75feada3289d81bf9
Kebron <kebisel21@gmail.com>
2026-03-29 19:50:46 +0300

    Time: 142 ms (18.98%), Space: 61.6 MB (20.43%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -2,17 +2,54 @@ class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         path = []
         res = []
-        def drive(num):
+        def trial(num):
             if len(path) == k:
                 res.append(path[:])
                 return
             for i in range(len(num)):
                 path.append(num[i])
-                drive(num[i + 1:])
+                trial(num[i + 1:])
                 path.pop()
-            return
-        arr=[i for i in range(1, n + 1)]
-        drive(arr)
-        return res 
+        trial([i for i in range(1 , n + 1)])
+        return res
+
+
+
+
+
+
+
+
+        # path = []
+        # res = []
+        # def trial(num):
+        #     if len(path ) == k:
+        #         res.append(path[:])
+        #         return 
+        #     for i in range(len(num)):
+        #         path.append(num[i])
+        #         trial(num[i+1:])
+        #         path.remove(num[i])
+        #     return 
+        # arr = [i for i in range(1, n + 1)]
+        # trial(arr)
+        # return res
+        
+
+
+        # path = []
+        # res = []
+        # def drive(num):
+        #     if len(path) == k:
+        #         res.append(path[:])
+        #         return
+        #     for i in range(len(num)):
+        #         path.append(num[i])
+        #         drive(num[i + 1:])
+        #         path.pop()
+        #     return
+        # arr=[i for i in range(1, n + 1)]
+        # drive(arr)
+        # return res 
 
         
