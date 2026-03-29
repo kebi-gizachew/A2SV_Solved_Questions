93ce0cacfa692c423ff6d6769ff8f396a3b31062
Kebron <kebisel21@gmail.com>
2026-03-29 20:02:59 +0300

    Time: 4 ms (10.29%), Space: 19.7 MB (22.84%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -2,16 +2,36 @@ class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         path = []
         res = []
-        def drive(num):
+        def trial(num):
             if len(nums) == len(path):
                 res.append(path[:])
                 return
             for i in range(len(num)):
                 path.append(num[i])
-                t = num[:i] + num[i + 1:]
-                drive(t)
+                trial(num[i + 1:] + num[:i])
                 path.pop()
-        drive(nums)
+        trial(nums)
         return res
+            
+
+
+
+
+
+
+
+        # path = []
+        # res = []
+        # def drive(num):
+        #     if len(nums) == len(path):
+        #         res.append(path[:])
+        #         return
+        #     for i in range(len(num)):
+        #         path.append(num[i])
+        #         t = num[:i] + num[i + 1:]
+        #         drive(t)
+        #         path.pop()
+        # drive(nums)
+        # return res
 
         
