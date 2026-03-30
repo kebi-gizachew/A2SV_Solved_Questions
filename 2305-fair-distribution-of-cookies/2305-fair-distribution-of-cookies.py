9b7f5b587d69574f8c4de035297350519eda88f7
Kebron <kebisel21@gmail.com>
2026-03-30 17:48:51 +0300

    Time: 7 ms (56.48%), Space: 19.4 MB (55.74%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
@@ -1,18 +1,18 @@
 class Solution:
     def distributeCookies(self, cookies: List[int], k: int) -> int:
         maxx = float("inf")
-        def trial(child , i , j):
+        def trial(arr, i , j):
             nonlocal maxx
             if i == len(cookies):
-                maxx = min(maxx, max(child))
+                maxx = min(maxx , max(arr))
                 return
             if j >= k:
                 return
-            child[j] += cookies[i]
-            trial(child , i + 1 , 0)
-            child[j] -= cookies[i]
-            if child[j] !=0:
-                trial(child , i , j + 1)
+            arr[j] += cookies[i]
+            trial(arr , i + 1 , 0)
+            arr[j] -= cookies[i]
+            if arr[j] != 0:
+                trial(arr , i , j + 1)
         trial([0 for i in range(k)] , 0 , 0)
         return maxx
 
@@ -20,15 +20,61 @@ class Solution:
 
 
 
-        # def trial(child , i):
+
+
+
+
+
+        # maxx = float("inf")
+        # def trial(arr , cookie):
+        #     nonlocal maxx
+        #     if cookie >= len(cookies):
+        #         maxx = min(maxx , max(arr))
+        #         return
+
+        #     for i in range(k):
+        #         arr[i] += cookies[cookie]
+        #         trial(arr , cookie + 1)
+        #         arr[i] -= cookies[cookie]
+        # trial([0 for i in range(k)] , 0) 
+        # return maxx
+
+
+
+
+
+
+
+
+        # maxx = float("inf")
+        # def trial(child , i , j):
         #     nonlocal maxx
         #     if i == len(cookies):
-        #         maxx = min(maxx , max(child))
+        #         maxx = min(maxx, max(child))
         #         return
-        #     for n in range(k):
-        #         child[n] += cookies[i]
-        #         trial(child , i + 1)
-        #         child[n] -= cookies[i]
+        #     if j >= k:
+        #         return
+        #     child[j] += cookies[i]
+        #     trial(child , i + 1 , 0)
+        #     child[j] -= cookies[i]
+        #     if child[j] !=0:
+        #         trial(child , i , j + 1)
+        # trial([0 for i in range(k)] , 0 , 0)
+        # return maxx
+
+
+
+
+
+        # # def trial(child , i):
+        # #     nonlocal maxx
+        # #     if i == len(cookies):
+        # #         maxx = min(maxx , max(child))
+        # #         return
+        # #     for n in range(k):
+        # #         child[n] += cookies[i]
+        # #         trial(child , i + 1)
+        # #         child[n] -= cookies[i]
           
         # trial([0 for i in range(k)] , 0)
         # return maxx
