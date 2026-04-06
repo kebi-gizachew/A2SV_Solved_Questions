a527389eb0611e4f2b394bdef04b1ec3e2eb107c
Kebron <kebisel21@gmail.com>
2026-04-06 20:11:25 +0300

    Time: 1435 ms (17.69%), Space: 19.4 MB (84.46%) - LeetHub

---
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.py
@@ -0,0 +1,110 @@
+class Solution:
+    def removeInvalidParentheses(self, s: str) -> List[str]:
+        res = set()
+        path = []
+        opened , closed = 0 , 0
+        for i in s:
+            if i == "(":
+                opened += 1
+            if i == ")":
+                if opened > 0:
+                    opened -=1
+                else:
+                    closed += 1
+        def trial(op , cl , left , right , index):
+            if index == len(s):
+                if left ==0 and right == 0:
+                    res.add("".join(path[:]))
+                return
+            if s[index] == "(" and op >0:
+                trial(op - 1, cl , left, right , index + 1)
+            if s[index] == ")" and cl > 0:
+                trial(op , cl - 1 , left ,right , index + 1)
+            if s[index] not in "()":
+                path.append(s[index])
+                trial(op , cl , left , right , index + 1)
+                path.pop()
+            elif s[index] == "(":
+                path.append(s[index])
+                trial(op , cl , left + 1 , right , index + 1)
+                path.pop()
+            elif s[index] == ")" and left > 0:
+                path.append(s[index])
+                trial(op , cl , left -1 , right , index + 1)
+                path.pop()
+        trial(opened, closed , 0 , 0 , 0)
+        return list(res)
+        
+
+
+
+
+
+
+
+
+
+
+
+        # res = set()
+        # l = r = 0
+        # for c in s:
+        #     if c == '(':
+        #         l += 1
+        #     elif c == ')':
+        #         if l > 0:
+        #             l -= 1
+        #         else:
+        #             r += 1
+        # def backtrack(index, left_count, right_count, l_rem, r_rem, path):
+        #     if index == len(s):
+        #         if l_rem == 0 and r_rem == 0:
+        #             res.add(path)
+        #         return
+            
+        #     char = s[index]            
+        #     if char == '(' and l_rem > 0:
+        #         backtrack(index + 1, left_count, right_count, l_rem - 1, r_rem, path)
+        #     elif char == ')' and r_rem > 0:
+        #         backtrack(index + 1, left_count, right_count, l_rem, r_rem - 1, path)
+            
+        #     if char not in "()":
+        #         backtrack(index + 1, left_count, right_count, l_rem, r_rem, path + char)
+        #     elif char == '(':
+        #         backtrack(index + 1, left_count + 1, right_count, l_rem, r_rem, path + char)
+        #     elif char == ')' and right_count < left_count:
+        #         backtrack(index + 1, left_count, right_count + 1, l_rem, r_rem, path + char)
+
+        # backtrack(0, 0, 0, l, r, "")
+        
+        # return list(res)
+
+
+
+
+
+
+
+        # path = []
+        # res = []
+        # def trial(st):
+        #     if not st:
+        #         stack = []
+        #         for t in path:
+        #             if t == "(":
+        #                 stack.append("(")
+        #             elif t == ")":
+        #                 if not stack:
+        #                     return
+        #                 else:
+        #                     stack.pop()
+        #         if not stack:
+        #             res.append("".join(path[:]))
+        #         return
+        #     for i in range(len(st)):
+        #         path.append(st[i])
+        #         trial(st[i + 1:])
+        #         path.pop()
+        # trial(s)
+        # return res
+        
