0e1020a10ba7cacb8d06a8d118d26a4291cbcf8e
Kebron <kebisel21@gmail.com>
2026-04-11 21:04:38 +0300

    Time: 1761 ms (5.2%), Space: 29.8 MB (7.16%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
@@ -2,18 +2,54 @@ class Solution:
     def getHappyString(self, n: int, k: int) -> str:
         path = []
         res = []
-        def trial():
+        def trial(letters , idx):
+            nonlocal n
             if len(path) == n:
-                for k in range(len(path) - 1 , 0 , -1):
-                    if path[k] == path[k - 1]:
+                for i in range(len(path) - 1):
+                    if path[i + 1] == path[i]:
                         return
-                res.append(path[:])
+                res.append("".join(path[:]))
                 return
-            for i in ["a" , "b" , "c"]:
-                path.append(i)
-                trial()
-                path.pop()
-        trial()
-        r = ["".join(i) for i in res]
-        r.sort()
-        return r[k - 1] if k -1 < len(r) else ""
+            if idx >= len(letters):
+                return
+            path.append(letters[idx])
+            trial(letters , 0)
+            path.pop()
+            trial(letters , idx + 1)
+        trial("abc" , 0)
+        res.sort()
+        return res[k- 1] if k - 1 < len(res) else ""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # path = []
+        # res = []
+        # def trial():
+        #     if len(path) == n:
+        #         for k in range(len(path) - 1 , 0 , -1):
+        #             if path[k] == path[k - 1]:
+        #                 return
+        #         res.append(path[:])
+        #         return
+        #     for i in ["a" , "b" , "c"]:
+        #         path.append(i)
+        #         trial()
+        #         path.pop()
+        # trial()
+        # r = ["".join(i) for i in res]
+        # r.sort()
+        # return r[k - 1] if k -1 < len(r) else ""
