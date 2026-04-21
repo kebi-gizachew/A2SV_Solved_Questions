62e6e3b7a5a33e3f81dd49529874b801369cbb33
Kebron <kebisel21@gmail.com>
2026-04-21 19:11:22 +0300

    Time: 9 ms (25.74%), Space: 22.4 MB (61.63%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -5,18 +5,26 @@ class Solution:
         """
         row = len(board)
         col = len(board[0])
-        def trial(i , j):
-            if i + 1 >= row or j + 1 >= col or i - 1 < 0 or j -1 <0 or board[i][j] == "X":
+        def fillBorder(i , j):
+            nonlocal row , col
+            if i < 0 or j <0 or i >= row or j >= col or board[i][j] != "O":
                 return
-            board[i][j] = "X"
-            trial(i + 1 , j)
-            trial(i - 1 , j)
-            trial(i , j + 1)
-            trial(i , j - 1)
+            board[i][j] = "F"
+            fillBorder(i + 1 , j)
+            fillBorder(i -1 , j)
+            fillBorder(i , j + 1)
+            fillBorder(i , j -1)
         for i in range(row):
             for j in range(col):
-                if board[i][j] == "O":
-                    trial(i , j)
-    
+                if (i == 0 or j == 0 or i == row - 1 or j == col - 1) and board[i][j] == "O":
+                    fillBorder(i , j)
+        for x in range(row):
+            for y in range(col):
+                if board[x][y] == "F":
+                    board[x][y] = "O"
+                elif board[x][y] == "O":
+                    board[x][y] = "X"
+        print(board)
+        
 
         
