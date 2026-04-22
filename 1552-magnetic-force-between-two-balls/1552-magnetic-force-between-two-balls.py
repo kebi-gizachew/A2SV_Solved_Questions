d4fe9c6d8ff78d7255aefb8d793a2225d3de5d0b
Kebron <kebisel21@gmail.com>
2026-04-23 01:46:14 +0300

    Time: 843 ms (5.07%), Space: 31.9 MB (26.93%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.py
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.py
@@ -3,25 +3,19 @@ class Solution:
         position.sort()
         left = 1
         right = position[-1] - position[0]
-        ans= 0 
+        ans = -1
         def checking(mid):
-            count = 1
-            element = position[0]
-            for i in range(len(position)):
-                if position[i] - element >= mid:
-                    count += 1
-                    element = position[i]
-            return count >= m
+            balls = 1
+            gap = 0
+            for t in range(1 , len(position)):
+                gap += position[t] - position[t - 1]
+                if gap >= mid:
+                    balls += 1
+                    gap = 0
+            return balls >= m
 
-            # count = 0
-            # for i in range(0, len(position) , mid):
-            #     count += 1
-            # if count >= m:
-            #     return True
-            # return False
-
-        while left <= right:
-            mid= left + (right - left)// 2
+        while left<= right:
+            mid = left + (right - left) // 2
             if checking(mid):
                 ans = mid
                 left = mid + 1
@@ -31,26 +25,52 @@ class Solution:
 
 
 
-
-
-
-        # return (max(position) - min(position)) // (m - 1)
         # position.sort()
-        # maxx = 0
-        # path = []
-        # def trial(nums):
-        #     nonlocal maxx
-        #     if len(path) == m:
-        #         minn = float("inf")
-        #         for t in range(len(path) - 1):
-        #             minn = min(abs(path[t] - path[t + 1]) ,minn)
-        #         maxx = max(maxx , minn)
-        #         return
+        # left = 1
+        # right = position[-1] - position[0]
+        # ans = 0
 
-        #     for i in range(len(nums)):
-        #         path.append(nums[i])
-        #         trial(nums[i + 1:])
-        #         path.pop()
-        # trial(position)
-        # return maxx
-        
+        # def checking(mid):
+        #     count = 1
+        #     element = position[0]
+        #     for i in range(len(position)):
+        #         if position[i] - element >= mid:
+        #             count += 1
+        #             element = position[i]
+        #     return count >= m
+
+        #     # count = 0
+        #     # for i in range(0, len(position) , mid):
+        #     #     count += 1
+        #     # if count >= m:
+        #     #     return True
+        #     # return False
+
+        # while left <= right:
+        #     mid = left + (right - left) // 2
+        #     if checking(mid):
+        #         ans = mid
+        #         left = mid + 1
+        #     else:
+        #         right = mid - 1
+        # return ans
+
+        # # return (max(position) - min(position)) // (m - 1)
+        # # position.sort()
+        # # maxx = 0
+        # # path = []
+        # # def trial(nums):
+        # #     nonlocal maxx
+        # #     if len(path) == m:
+        # #         minn = float("inf")
+        # #         for t in range(len(path) - 1):
+        # #             minn = min(abs(path[t] - path[t + 1]) ,minn)
+        # #         maxx = max(maxx , minn)
+        # #         return
+
+        # #     for i in range(len(nums)):
+        # #         path.append(nums[i])
+        # #         trial(nums[i + 1:])
+        # #         path.pop()
+        # # trial(position)
+        # # return maxx
