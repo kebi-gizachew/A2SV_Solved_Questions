b264d75dc578ef61955dc0ad84119fa1a82d1347
Kebron <kebisel21@gmail.com>
2026-04-23 23:07:07 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -0,0 +1,69 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        directions = [(-1, 0), (0 , -1) , (1 , 0), (0, 1)]
+        res = []
+        pacific = set()
+        atlantic = set()
+        def ifPass(i , j , visit, val):
+            if i < 0 or j < 0 or i >= len(heights) or j >= len(heights[0]) or heights[i][j] < val or (i , j) in visit:
+                return
+            visit.add((i , j))
+            for x , y in directions:
+                ifPass(i + x , j + y , visit, heights[i][j])
+        for j in range(len(heights[0])):
+            ifPass(0 , j , pacific , heights[0][j])
+            ifPass(len(heights)- 1 , j , atlantic , heights[len(heights) - 1][j])
+        for i in range(len(heights)):
+            ifPass(i , 0 , pacific , heights[i][0])
+            ifPass(i , len(heights) - 1 , atlantic , heights[i][len(heights) - 1])
+        for x , y in pacific:
+            if (x , y) in atlantic:
+                res.append([x , y])
+        return res
+
+            
+
+
+
+
+
+
+
+
+
+
+
+        # pacific = set()
+        # atlantic = set()
+        # directions = [(-1 , 0) , (1 , 0) , (0 , -1) , (0 , 1)]
+        # def ifPass(i , j , val):
+        #     if i < 0 or j < 0:
+        #         return True
+        #     if j >= len(heights[0]) or i >=len(heights[0]):
+        #         return False
+        #     for x , y in directions:
+        #         if ifpass(i + x , j + y):
+        #             pacific.add((i , j))
+        #         else:
+        #             atlantic.add((i , j))
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
