4f506ac15d82443ec3c97626d6fb765d416d466c
Kebron <kebisel21@gmail.com>
2026-04-27 12:15:58 +0300

    Time: 3 ms (38.11%), Space: 20.9 MB (5.56%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -1,28 +1,69 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        black = set()
-        white = set()
-        def traverse(i , flag):
-            if i in black:
-                return flag == 0
-            if i in white:
-                return flag == 1
-            
-            if flag == 1:
-                white.add(i)
-                flag = 0
-            else:
-                black.add(i)
-                flag = 1
-           
-            for row in graph[i]:
-                if not traverse(row , flag):
+        white , black = 1 , 0
+        d = {i : -1 for i in range(len(graph))}
+        def cycleCycling(val , Color):
+            if d[val] == Color:
+                return True
+            elif d[val] == 1 - Color:
+                return False
+            d[val] = Color
+            for t in graph[val]:
+                if not cycleCycling(t , 1 - Color):
                     return False
             return True
-        for i in range(len(graph)):
-            if i not in black and i not in white:
-                if not traverse(0 , 1):
+        for y in range(len(graph)):
+            if d[y] == -1:
+                if not cycleCycling(y , white):
                     return False
         return True
+
+
+
+
+
+
+
+
+
+
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+        # black = set()
+        # white = set()
+        # def traverse(i , flag):
+        #     if i in black:
+        #         return flag == 0
+        #     if i in white:
+        #         return flag == 1
+            
+        #     if flag == 1:
+        #         white.add(i)
+        #         flag = 0
+        #     else:
+        #         black.add(i)
+        #         flag = 1
+           
+        #     for row in graph[i]:
+        #         if not traverse(row , flag):
+        #             return False
+        #     return True
+        # for i in range(len(graph)):
+        #     if i not in black and i not in white:
+        #         if not traverse(i , 1):
+        #             return False
+        # return True
 
         
