3b3f26bcbf3b37075ad66c30cdb4c16dd1b361b0
Kebron <kebisel21@gmail.com>
2026-05-01 22:39:20 +0300

    Time: 0 ms (100%), Space: 20 MB (7.41%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,13 +1,17 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         directions = [(-1 , 0) , (1 , 0) , (0 , -1) , (0, 1)]
-        def changeColor(i , j):
+        def changeColor(i , j, type1):
             nonlocal color
-            if 0 <= i < len(image) and 0 <= j < len(image[0]) and image[i][j] == 1:
-                image[i][j] = color
+            if 0 <= i < len(image) and 0 <= j < len(image[0]) and image[i][j] == type1:
+                image[i][j] = str(color)
                 for dirX, dirY in directions:
-                    changeColor(dirX + i , dirY + j)
-        changeColor(sr, sc)
+                    changeColor(dirX + i , dirY + j, type1)
+        changeColor(sr, sc, image[sr][sc])
+        for i in range(len(image)):
+            for j in range(len(image[0])):
+                if image[i][j] == str(color):
+                    image[i][j] = color
         return image
 
         
