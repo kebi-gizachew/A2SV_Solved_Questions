465e84f85bf6658d71f5a623a11d2f3b5905ab31
Kebron <kebisel21@gmail.com>
2026-05-07 00:51:35 +0300

    Time: 4 ms (48.16%), Space: 21.2 MB (26.66%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,20 +1,72 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         d = defaultdict(list)
-        indegree = [0] * numCourses
+        color = [0 for _ in range(numCourses)]
+        res = []
         for x, y in prerequisites:
             d[y].append(x)
-            indegree[x]+= 1
-        queue = deque([i for i in range(numCourses) if indegree[i] == 0])
-        res = []
-        while queue:
-            temp = queue.popleft()
-            res.append(temp)
-            for t in d[temp]:
-                indegree[t] -= 1
-                if indegree[t] == 0:
-                    queue.append(t)
-        return res if len(res) == numCourses else []
+        def dfs(node):
+            if color[node] == 1:
+                return False
+            if color[node] == 2:
+                return True
+            color[node] = 1
+            for i in d[node]:
+                if not dfs(i):
+                    return False
+            res.append(node)
+            color[node] = 2
+            return True
+        for i in range(numCourses):
+            if not dfs(i):
+                return []
+        return res[::-1]
+
+        
+
+
+
+        # color = [0 for i in range(numCourses)]
+        # d= defaultdict(list)
+        # res = []
+        # for x, y in prerequisites:
+        #     d[y].append(x)
+        # def dfs(node):
+        #     if color[node] == 1:
+        #         return False
+        #     if color[node] == 2:
+        #         return True
+        #     color[node] = 1
+        #     for k in d[node]:
+        #         if not dfs(k):
+        #             return False
+        #     res.append(node)
+        #     color[node] = 2
+        #     return True
+        # for num in range(numCourses):
+        #     if color[num] == 0 and not dfs(num):
+        #         return []
+        # return res[::-1]
+
+
+
+
+        # d = defaultdict(list)
+        # indegree = [0] * numCourses
+        # for x, y in prerequisites:
+        #     d[y].append(x)
+        #     indegree[x]+= 1
+        # queue = deque([i for i in range(numCourses) if indegree[i] == 0])
+        # res = []
+        # while queue:
+        #     temp = queue.popleft()
+        #     res.append(temp)
+        #     for t in d[temp]:
+        #         indegree[t] -= 1
+        #         if indegree[t] == 0:
+        #             queue.append(t)
+        # return res if len(res) == numCourses else []
+        # it is going to be empty if there is a cycle becuause at some point where the whole indgreee is notzero and the queue is empty it will return res before fully iterating
     
         
 
