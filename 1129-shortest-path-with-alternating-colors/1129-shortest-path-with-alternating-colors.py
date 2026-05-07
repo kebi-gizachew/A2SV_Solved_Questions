8adfcdf2ba1c1fda0322fd33c6c77511e0673862
Kebron <kebisel21@gmail.com>
2026-05-08 00:52:58 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
@@ -1,33 +1,87 @@
 class Solution:
     def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
-        redD = defaultdict(list)
-        blueD = defaultdict(list)
+        red = defaultdict(list)
+        blue= defaultdict(list)
         for x, y in redEdges:
-            redD[x].append(y)
-        for x,y in blueEdges:
-            blueD[x].append(y)
-        bans , rans = [float("inf")]*n , [float("inf")]*n
-        queue = deque([(0 , "r") , (0 , "b")])
-        bans[0] = rans[0] = 0
+            red[x].append(y)
+        for x, y in blueEdges:
+            blue[x].append(y)
+        redd = [float("inf") for _ in range(n)]
+        bluee = [float("inf") for _ in range(n)]
+        redd[0] , bluee[0] = 0 , 0
+        queue = deque([(0 , "red"), (0 , "blue")])
         while queue:
-            temp , color = queue.popleft()
-            if color == "b":
-                for t in redD[temp]:
-                    if bans[temp] + 1 < rans[t]:
-                        rans[t] = bans[temp] + 1
-                        queue.append((t , "r"))
-            else:
-                for t in blueD[temp]:
-                    if rans[temp] + 1 < bans[t]:
-                        bans[t] = rans[temp] + 1
-                        queue.append((t , "b"))
-        ans = []
+            for i in range(len(queue)):
+                val , color = queue.popleft()
+                if color == "red":
+                    for t in blue[val]:
+                        bluee[t] = min(bluee[t] , redd[val] + 1)
+                        queue.append((t , "blue"))
+                else:
+                    for t in red[val]:
+                        redd[t] = min(redd[t] , bluee[val] + 1)
+                        queue.append((t, "red"))
+        res= []
         for t in range(n):
-            r = min(bans[t] , rans[t])
-            if r  == float("inf"):ans.append(-1)
+            minn = min(redd[t] , bluee[t])
+            if minn == float("inf"):
+                res.append(-1)
             else:
-                ans.append(r)
-        return ans
+                res.append(minn)
+        return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # redD = defaultdict(list)
+        # blueD = defaultdict(list)
+        # for x, y in redEdges:
+        #     redD[x].append(y)
+        # for x,y in blueEdges:
+        #     blueD[x].append(y)
+        # bans , rans = [float("inf")]*n , [float("inf")]*n
+        # queue = deque([(0 , "r") , (0 , "b")])
+        # bans[0] = rans[0] = 0
+        # while queue:
+        #     temp , color = queue.popleft()
+        #     if color == "b":
+        #         for t in redD[temp]:
+        #             if bans[temp] + 1 < rans[t]:
+        #                 rans[t] = bans[temp] + 1
+        #                 queue.append((t , "r"))
+        #     else:
+        #         for t in blueD[temp]:
+        #             if rans[temp] + 1 < bans[t]:
+        #                 bans[t] = rans[temp] + 1
+        #                 queue.append((t , "b"))
+        # ans = []
+        # for t in range(n):
+        #     r = min(bans[t] , rans[t])
+        #     if r  == float("inf"):ans.append(-1)
+        #     else:
+        #         ans.append(r)
+        # return ans
                     
 
 
