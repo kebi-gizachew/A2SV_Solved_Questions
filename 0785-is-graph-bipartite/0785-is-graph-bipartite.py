c7ef12acec47aa782489414d537f118d0289f184
Kebron <kebisel21@gmail.com>
2026-05-08 15:06:37 +0300

    Time: 7 ms (11.87%), Space: 21.4 MB (12.67%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -1,22 +1,62 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        white , black = 1 , 0
-        d = {i : -1 for i in range(len(graph))}
-        def cycleCycling(val , Color):
-            if d[val] == Color:
-                return True
-            elif d[val] == 1 - Color:
+        color = [0 for _ in range(len(graph))]
+        d = defaultdict(list)
+        visited = set()
+        for i in range(len(graph)):
+            for j in graph[i]:
+                d[i].append(j)
+        def dfs(node, cur):
+            if color[node] == 1 and cur == 2:
                 return False
-            d[val] = Color
-            for t in graph[val]:
-                if not cycleCycling(t , 1 - Color):
+            if color[node] == 2 and cur == 1:
+                return False
+            if node in visited or color[node] == cur:
+                return True
+            color[node] = cur
+            cur = 2 if cur == 1 else 1
+            for t in d[node]:
+                if not dfs(t , cur):
                     return False
+            visited.add(node)
             return True
-        for y in range(len(graph)):
-            if d[y] == -1:
-                if not cycleCycling(y , white):
-                    return False
+        for i in range(len(graph)):
+            if color[i] == 0 and not dfs(i, 1):
+                return False
         return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # white , black = 1 , 0
+        # d = {i : -1 for i in range(len(graph))}
+        # def cycleCycling(val , Color):
+        #     if d[val] == Color:
+        #         return True
+        #     elif d[val] == 1 - Color:
+        #         return False
+        #     d[val] = Color
+        #     for t in graph[val]:
+        #         if not cycleCycling(t , 1 - Color):
+        #             return False
+        #     return True
+        # for y in range(len(graph)):
+        #     if d[y] == -1:
+        #         if not cycleCycling(y , white):
+        #             return False
+        # return True
 
 
 
