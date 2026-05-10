007d19f08ce32effa2d6bdd77b5c7be316278231
Kebron <kebisel21@gmail.com>
2026-05-10 21:15:06 +0300

    Time: 71 ms (94.1%), Space: 39.9 MB (97.53%) - LeetHub

--- 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.py
+++ 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.py
@@ -1,22 +1,38 @@
 class Solution:
     def minSubarray(self, nums: List[int], p: int) -> int:
-        total= sum(nums)
-        remain = total % p
-        if total < p:
-            return -1
-        if remain ==0 :
-            return 0
-        print(remain)
+        d = {0 :-1}
+        target = sum(nums) % p
         summ = 0
-        c= {0:-1}
-        op= len(nums)
-        for r in range(len(nums)):
-            summ =(summ +nums[r]) % p
-            temp = (summ - remain +p) % p
-            if temp in c:
-                op = min(op , r- c[temp])
-            c[summ] = r
-        if op == len(nums):
+        if target == 0:
+            return 0
+        if sum(nums) < p:
             return -1
-        return op
+        minn = len(nums)
+        for i in range(len(nums)):
+            summ += nums[i]
+            temp = summ % p
+            val = (p - target + temp) %p
+            if val in d:
+                minn = min(i - d[val], minn)
+            d[temp] = i
+        return minn if minn != len(nums) else -1
+        # total= sum(nums)
+        # remain = total % p
+        # if total < p:
+        #     return -1
+        # if remain ==0 :
+        #     return 0
+        # print(remain)
+        # summ = 0
+        # c= {0:-1}
+        # op= len(nums)
+        # for r in range(len(nums)):
+        #     summ =(summ +nums[r]) % p
+        #     temp = (summ - remain +p) % p
+        #     if temp in c:
+        #         op = min(op , r- c[temp])
+        #     c[summ] = r
+        # if op == len(nums):
+        #     return -1
+        # return op
         
