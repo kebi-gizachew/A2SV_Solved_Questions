1d082d4bbcf62690bc1960d4dda4c5fd9ac4415b
Kebron <kebisel21@gmail.com>
2026-05-23 14:55:56 +0300

    Time: 16 ms (11.55%), Space: 12.4 MB (57.3%) - LeetHub - Time: O, Space: O(?)

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -10,23 +10,24 @@ class Solution(object):
         :type l2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        t=ListNode()
-        y=t
-        c=0
-        while l1 or l2 or c:
-            v1=l1.val if l1 else 0
-            v2=l2.val if l2 else 0
-            total=v1+v2+c
-            c=total//10
-            y.next=ListNode(total%10)
-            y=y.next
+        carry = 0
+        dummy = ListNode(0)
+        cur = dummy
+        while l1 or l2 or carry:
+            l1v = l1.val if l1 else 0
+            l2v = l2.val if l2 else 0
+            total = l1v + l2v + carry
+            carry = total // 10
+            val = total %10
+            cur.next = ListNode(val)
+            cur = cur.next
             if l1:
-                l1=l1.next
+                l1= l1.next
             if l2:
-                l2=l2.next 
-        return t.next
-
-
-
+                l2 = l2.next
+        return dummy.next
         
-        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
