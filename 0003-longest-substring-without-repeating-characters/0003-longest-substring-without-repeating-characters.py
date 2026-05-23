ecf18012957f8d81a217397c84d4154d8adfbc02
Kebron <kebisel21@gmail.com>
2026-05-23 15:09:07 +0300

    Time: 12 ms (55.95%), Space: 19.3 MB (30.75%) - LeetHub - Time: O(?), Space: O(?)

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,23 +1,55 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        arr=Counter()
-        l = 0
-        op = 0 
-        for i in range(len(s)):
-            if s[i] in arr:
-                op = max(op , i - l)
-                while s[l] != s[i]:
-                    del arr[s[l]]
-                    l+= 1
-                if l!=i:
-                    l+=1
-            else:
-                arr[s[i]] += 1
-        op = max(op , len(arr))
-        return op
+        start = 0
+        charStore = defaultdict(int)
+        maxx = 0
+        for t in range(len(s)):
+            if s[t] in charStore:
+                tempChar = charStore[s[t]]
+                if start <= tempChar:
+                    start = tempChar + 1
+            maxx = max(maxx , t - start + 1)
+            charStore[s[t]] = t
+        return maxx
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # arr=Counter()
+        # l = 0
+        # op = 0 
+        # for i in range(len(s)):
+        #     if s[i] in arr:
+        #         op = max(op , i - l)
+        #         while s[l] != s[i]:
+        #             del arr[s[l]]
+        #             l+= 1
+        #         if l!=i:
+        #             l+=1
+        #     else:
+        #         arr[s[i]] += 1
+        # op = max(op , len(arr))
+        # return op
 
 
             
 
 
         
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
