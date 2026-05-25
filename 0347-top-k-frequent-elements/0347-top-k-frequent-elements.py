da132fe3968f6d0e4437f44850dbd3629ac0eaca
Kebron <kebisel21@gmail.com>
2026-05-26 00:35:26 +0300

    Time: 7 ms (51.76%), Space: 22.9 MB (62.04%) - LeetHub - Time: O(n log n), Space: O(n)

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,12 +1,50 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        rel=Counter(nums)
-        arr=[]
-        for num,count in rel.items():
-            arr.append([count,num])
-        arr.sort()
-        r=[]
-        while len(r)<k:
-            r.append(arr.pop()[1])        
-        return r
+        count = Counter(nums)
+        heap= []
+        for x , y in count.items():
+            heap.append([y , x])
+        heapify(heap)
+        while len(heap) > k:
+            heappop(heap)
+        res = []
+        for i , j in heap:
+            res.append(j)
+        return res
+
+
+
+
+        # count = Counter(nums)
+        # bucket = [[] for t in range(len(nums) + 1)]
+        # for t , freq in count.items():
+        #     bucket[freq].append(t)
+        # res = []
+        # for i in range(len(nums), 0 , -1):
+        #     for t in bucket[i]:
+        #         res.append(t)
+        #         if len(res) == k:
+        #             return res
+
+
+
+
+
+
+
+
+
+        # rel=Counter(nums)
+        # arr=[]
+        # for num,count in rel.items():
+        #     arr.append([count,num])
+        # arr.sort()
+        # r=[]
+        # while len(r)<k:
+        #     r.append(arr.pop()[1])        
+        # return r
         
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
