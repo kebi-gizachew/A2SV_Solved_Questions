97542aef8b44e5568698b3a2ff1f397fdd0309de
Kebron <kebisel21@gmail.com>
2026-05-25 23:36:14 +0300

    Time: 1 ms (22.3%), Space: 19.3 MB (71.6%) - LeetHub - Time: O(n log n), Space: O(n)

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -1,18 +1,47 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
         heap = []
-        for t in stones:
-            heappush(heap, -t)
-        while len(heap) >= 1:
-            if len(heap) == 1:
-                return -(heap[0])
-            largest = -(heappop(heap))
-            second = 0
-            if heap:
-                second = -(heappop(heap))
-            if second != largest:
-                heappush(heap, second - largest)
+        for i in stones:
+            heappush(heap, -i)
+        while len(heap) > 1:
+            v1 = -heappop(heap)
+            v2 = -heappop(heap)
+            if v1 != v2:
+                heappush(heap, -abs(v1 - v2))
+        if heap:
+            return -heap[0]
         return 0
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # heap = []
+        # for t in stones:
+        #     heappush(heap, -t)
+        # while len(heap) >= 1:
+        #     if len(heap) == 1:
+        #         return -(heap[0])
+        #     largest = -(heappop(heap))
+        #     second = 0
+        #     if heap:
+        #         second = -(heappop(heap))
+        #     if second != largest:
+        #         heappush(heap, second - largest)
+        # return 0
 
 
 # Synced seamlessly with LeetHub Pro
