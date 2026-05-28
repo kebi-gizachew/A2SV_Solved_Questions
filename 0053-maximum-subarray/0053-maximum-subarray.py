67e5b0a6613a4cdd552467766573f128f06f51ad
Kebron <kebisel21@gmail.com>
2026-05-29 01:01:11 +0300

    Time: 48 ms (20.12%), Space: 31.5 MB (44.29%) - LeetHub - Time: O(?), Space: O(?)

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,11 +1,66 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        maxx = 0
-        cur = nums[0]
-        for r in range(len(nums)):
-            maxx = max(nums[r] ,maxx + nums[r])
-            cur = max(cur , maxx)
-        return cur
+        minn = 0
+        summ = 0
+        maxx = nums[0]
+        for i in range(len(nums)):
+            summ += nums[i]
+            maxx = max(maxx , summ - minn)
+            minn = min(minn , summ)
+        return maxx
+        # maxx = nums[0]
+        # cur = nums[0]
+        # for i in range(len(nums)):
+        #     cur = max(cur + nums[i] , nums[i])
+        #     maxx = max(maxx , cur)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # maxx = nums[0]
+        # cur = 0
+        # for i in range(len(nums)):
+        #     cur = max(nums[i] , cur + nums[i])
+        #     maxx = max(maxx , cur)
+        # return maxx
+
+        # minn = 0
+        # summ = 0
+        # maxx = 0
+        # for i in range(len(nums)):
+        #     summ += nums[i]
+        #     maxx = max(maxx , summ - minn)
+        #     minn = min(minn , summ)
+        # return maxx
+
+
+
+
+
+
+
+        # # maxx = 0
+        # # cur = nums[0]
+        # # for r in range(len(nums)):
+        # #     maxx = max(nums[r] ,maxx + nums[r])
+        # #     cur = max(cur , maxx)
+        # # return cur
+
+
+
+
+
         # summ = 0
         # finale = float("-inf")
         # minn = 0
@@ -15,3 +70,7 @@ class Solution:
         #     minn = min(minn , summ)
         # return finale
         
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
