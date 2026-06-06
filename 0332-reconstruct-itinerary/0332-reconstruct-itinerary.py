fdc8e3bdebe46f8dddf94c30e3b32ce3b1c355ee
Kebron <kebisel21@gmail.com>
2026-06-06 21:57:36 +0300

    Time: 3 ms (77.26%), Space: 20.1 MB (11.13%) - LeetHub - Time: O(?), Space: O(?)

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -1,19 +1,49 @@
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
         d = defaultdict(list)
-        for x, y in tickets:
-            heappush(d[x], y)
         res = []
-        def dfs(val):
-            while d[val]:
-                temp = heappop(d[val])
+        for x , y in tickets:
+            heappush(d[x], y)
+        def dfs(node):
+            if not node:
+                return
+            while d[node]:
+                temp = heappop(d[node])
                 dfs(temp)
-            res.append(val)
+            res.append(node)
         dfs("JFK")
         return res[::-1]
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # d = defaultdict(list)
+        # for x, y in tickets:
+        #     heappush(d[x], y)
+        # res = []
+        # def dfs(val):
+        #     while d[val]:
+        #         temp = heappop(d[val])
+        #         dfs(temp)
+        #     res.append(val)
+        # dfs("JFK")
+        # return res[::-1]
 
 
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
